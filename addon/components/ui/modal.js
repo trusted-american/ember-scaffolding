d4d8550f760be1cde27bfe0c423e95672498d176
Charles Fries <charliefries@icloud.com>
2020-09-05 03:07:30 -0700

    Ch

--- addon/components/ui/modal.js
+++ addon/components/ui/modal.js
@@ -1,4 +1,38 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+/* global bootstrap */
 
 export default class UiModalComponent extends Component {
+
+	/* 
+	 * ARGS
+	 * 
+	 * title?: string?
+	 * size?: 'sm'|'lg'|'xl'
+	 * onClose: Function
+	 * 
+	 */
+
+	modal;
+
+	@action didInsert(el) {
+		this.modal = new bootstrap.Modal(el);
+		this.modal.show();
+
+		el.addEventListener('shown.bs.modal', () => {
+			let autofocus = el.querySelector('[autofocus]');
+			if (autofocus) {
+				autofocus.focus();
+			}
+		});
+
+		el.addEventListener('hidden.bs.modal', () => {
+			this.args.onClose();
+		});
+	}
+
+	willDestroy() {
+		this.modal.hide();
+		// this.modal.dispose();
+	}
 }
