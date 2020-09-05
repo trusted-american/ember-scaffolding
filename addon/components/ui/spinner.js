5f383389a02bf98dad3fd2f1b14e69357083bb60
Charles Fries <charliefries@icloud.com>
2020-09-05 03:02:12 -0700

    Comp

---
+++ addon/components/ui/spinner.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+
+/* 
+ * ARGS
+ * 
+ * large?: boolean
+ * centered?: boolean
+ * 
+ */
+
+export default class UiSpinnerComponent extends Component {
+}
