4ee80ab45e0ef9e8c6915acdc0dc96ddb157b335
Charles Fries <charliefries@icloud.com>
2020-09-09 09:20:56 -0700

    Generate MiscConfirmationDialogComponent

---
+++ addon/components/misc/confirmation-dialog.js
@@ -0,0 +1,17 @@
+import Component from '@glimmer/component';
+
+/* 
+ * ARGS
+ * 
+ * title: string
+ * confirmTitle: string
+ * loading?: boolean
+ * disabled?: boolean
+ * color?: string = 'danger'
+ * onConfirm: Function
+ * onCancel: Function
+ * 
+ */
+
+export default class MiscConfirmationDialogComponent extends Component {
+}
