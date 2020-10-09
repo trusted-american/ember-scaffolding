b806ece8d157912f4064a7cb028fa86d293022b2
Charles Fries <charliefries@icloud.com>
2020-10-08 17:00:38 -0700

    Bump version to 1.0.23

--- addon/helpers/currency.js
+++ addon/helpers/currency.js
@@ -1,6 +1,6 @@
 import { helper } from '@ember/component/helper';
 
-export default helper(function currency([amount]) {
-	amount /= 100;
+export default helper(function currency([amount, cents = false]) {
+	if (cents) amount /= 100;
 	return amount.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
 });
