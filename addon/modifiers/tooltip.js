078fde4c6d44892f32fff039d763e08d78518028
Charles Fries <charliefries@icloud.com>
2021-01-30 03:02:13 -0800

    Fix tooltips, bump version to 1.1.5

--- addon/modifiers/tooltip.js
+++ addon/modifiers/tooltip.js
@@ -2,15 +2,15 @@ import { modifier } from 'ember-modifier';
 /* global bootstrap */
 
 export default modifier(function tooltip(element, [title, placement = 'top']) {
-	element.setAttribute('data-toggle', 'tooltip');
-	element.setAttribute('data-placement', placement);
+	element.setAttribute('data-bs-toggle', 'tooltip');
+	element.setAttribute('data-bs-placement', placement);
 	element.setAttribute('title', title);
 
 	new bootstrap.Tooltip(element);
 
 	return () => {
-		element.removeAttribute('data-toggle');
-		element.removeAttribute('data-placement');
+		element.removeAttribute('data-bs-toggle');
+		element.removeAttribute('data-bs-placement');
 		element.removeAttribute('title');
 	};
 });
