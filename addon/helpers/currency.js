bc4d06858839483d325699e88d2385a7ab0930c5
Charles Fries <charliefries@icloud.com>
2021-03-24 13:16:24 -0700

    Add accounting format to currency helper

--- addon/helpers/currency.js
+++ addon/helpers/currency.js
@@ -1,9 +1,19 @@
 import { helper } from '@ember/component/helper';
 
-export default helper(function currency([amount, cents = false]) {
+export default helper(function currency([amount, cents = false, accountingStyle = false]) {
 	if (!amount && amount !== 0) {
 		return null;
 	}
 	if (cents) amount /= 100;
+
+	if (accountingStyle) {
+		let absValue = Math.abs(amount);
+		let returnString = Number(absValue).toLocaleString('en-US', {
+			currency: 'USD',
+			minimumFractionDigits: 2,
+		});
+		return amount < 0 ? '(' + returnString + ')' : returnString;
+	}
+
 	return amount.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
 });
