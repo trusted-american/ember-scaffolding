d83c398acd99536c6ffafbf7a7763ca8830857de
Charles Fries <charliefries@icloud.com>
2021-03-29 11:36:08 -0700

    Generate UiOffcanvasComponent, offcanvas modifier

---
+++ addon/components/ui/offcanvas.js
@@ -0,0 +1,10 @@
+import Component from '@glimmer/component';
+
+/**
+ * ARGS
+ * title: string
+ * placement?: 'start'|'end'|'bottom'
+ */
+
+export default class UiOffcanvasComponent extends Component {
+}
