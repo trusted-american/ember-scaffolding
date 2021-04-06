db752ed033f7bd94471970844843f2aab44ac86b
Charles Fries <charliefries@icloud.com>
2021-04-06 12:32:52 -0700

    Generate UiDropdownItemComponent

---
+++ addon/components/ui/dropdown/item.js
@@ -0,0 +1,20 @@
+import Component from '@glimmer/component';
+import { assert } from '@ember/debug';
+import { typeOf } from '@ember/utils';
+
+/**
+ * ARGS
+ * label: boolean
+ * color?: string
+ * icon?: string
+ * disabled?: boolean
+ * onClick: Function
+ */
+
+export default class UiDropdownItemComponent extends Component {
+	constructor() {
+		super(...arguments);
+		assert('<Ui::Dropdown::Item />: Must pass a label string', typeOf(this.args.label) === 'string');
+		assert('<Ui::Dropdown::Item />: Must pass an onClick function', typeOf(this.args.onClick) === 'function');
+	}
+}
