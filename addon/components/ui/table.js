9332099d6f3183d2c47d2d195d355f7c0a864236
Charles Fries <charliefries@icloud.com>
2021-04-19 17:05:11 -0700

    Add pageNumber requirement

--- addon/components/ui/table.js
+++ addon/components/ui/table.js
@@ -1,11 +1,18 @@
 import Component from '@glimmer/component';
+import { assert } from '@ember/debug';
+import { typeOf } from '@ember/utils';
 
 /**
  * ARGS
  * data: object[]
  * pagination?: boolean = true
  * pageSize?: number = 25
+ * pageNumber: number
  */
 
 export default class UiTableComponent extends Component {
+	constructor() {
+		super(...arguments);
+		assert('<Ui::Table />: Must pass a pageNumber number', typeOf(this.args.pageNumber) === 'number');
+	}
 }
