860f914af1964f03356fb1c4ee1c587340cbaff3
Charles Fries <charliefries@icloud.com>
2021-05-24 11:11:51 -0700

    Generate percentage, age helpers

---
+++ addon/helpers/percentage.js
@@ -0,0 +1,8 @@
+import { helper } from '@ember/component/helper';
+
+export default helper(function percentage([number], { decimals }) {
+	let value = (number || 0) * 100;
+	let numDecimals = decimals || 0;
+
+	return `${value.toFixed(value % 1 === 0 ? 0 : numDecimals)}%`;
+});
