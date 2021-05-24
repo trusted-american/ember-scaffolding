9c351688c432b7b7c02a0cff78054e8436660044
Charles Fries <charliefries@icloud.com>
2021-05-24 11:10:07 -0700

    Update timestamp helper

--- addon/helpers/timestamp.js
+++ addon/helpers/timestamp.js
@@ -2,10 +2,18 @@ import { helper } from '@ember/component/helper';
 import { typeOf } from '@ember/utils';
 import moment from 'moment';
 
-export default helper(function timestamp([date, format]) {
+export default helper(function timestamp([date, format], { number, year }) {
 	if (!date) return null;
 
+	if (year) {
+		return new Date(date).getFullYear();
+	}
+
 	let mom = typeOf(date) === 'number' ? moment.unix(date) : moment(date);
+
+	if (number) {
+		return mom.format('MM/DD/YYYY');
+	}
 
 	if (format === 'date') {
 		return mom.format('MMM D, Y');
