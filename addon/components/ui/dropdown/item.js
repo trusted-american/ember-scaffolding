08e08001b2e9ae042db039302d8637aeff7b95dc
Charles Fries <charliefries@icloud.com>
2021-06-07 13:49:56 -0700

    Allow block for UiDropdownItemComponent

--- addon/components/ui/dropdown/item.js
+++ addon/components/ui/dropdown/item.js
@@ -4,7 +4,7 @@ import { typeOf } from '@ember/utils';
 
 /**
  * ARGS
- * label: boolean
+ * label?: boolean
  * color?: string
  * icon?: string
  * disabled?: boolean
@@ -14,7 +14,7 @@ import { typeOf } from '@ember/utils';
 export default class UiDropdownItemComponent extends Component {
 	constructor() {
 		super(...arguments);
-		assert('<Ui::Dropdown::Item />: Must pass a label string', typeOf(this.args.label) === 'string');
+		// assert('<Ui::Dropdown::Item />: Must pass a label string', typeOf(this.args.label) === 'string');
 		assert('<Ui::Dropdown::Item />: Must pass an onClick function', typeOf(this.args.onClick) === 'function');
 	}
 }
