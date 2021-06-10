3bcd676d07b5ab85fdc3603b7d9b0abfb63e1cc7
Charles Fries <charliefries@icloud.com>
2021-06-09 20:33:18 -0700

    Add toLocaleString percentage calc

--- addon/helpers/percentage.js
+++ addon/helpers/percentage.js
@@ -4,5 +4,12 @@ export default helper(function percentage([number], { decimals }) {
 	let value = (number || 0) * 100;
 	let numDecimals = decimals || 0;
 
+	// let valueStr = value.toLocaleString(undefined, {
+	// 	minimumFractionDigits: numDecimals,
+	// 	maximumFractionDigits: numDecimals,
+	// });
+
+	// return `${valueStr}%`;
+
 	return `${value.toFixed(value % 1 === 0 ? 0 : numDecimals)}%`;
 });
