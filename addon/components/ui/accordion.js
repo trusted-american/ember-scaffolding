8ca1f8ba93b0fb6eadcd07afee9ccf3ba66ef3c0
Charles Fries <charliefries@icloud.com>
2021-07-10 13:19:23 -0700

    Add alwaysOpen accordion

--- addon/components/ui/accordion.js
+++ addon/components/ui/accordion.js
@@ -3,6 +3,11 @@ import UiBaseComponent from './base';
 /**
  * ARGS
  * flush?: boolean
+ * alwaysOpen?: boolean
  */
 
-export default class UiAccordionComponent extends UiBaseComponent {}
+export default class UiAccordionComponent extends UiBaseComponent {
+	get alwaysOpen() {
+		return this.args.alwaysOpen;
+	}
+}
